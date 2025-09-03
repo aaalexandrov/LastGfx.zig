8902486b8f2852e3b6333566b9ad4cf8a0386a79
Lolla Bunny <greyalex2003@yahoo.com>
2025-09-03 03:18:09 +0300

    SDL3 window

---
+++ src/main.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+});
+
+pub fn main() !void {
+    try errify(c.SDL_Init(c.SDL_INIT_VIDEO));
+    defer c.SDL_Quit();
+
+    const window = c.SDL_CreateWindow("LastGfx", 400, 300, c.SDL_WINDOW_RESIZABLE);
+    defer c.SDL_DestroyWindow(window);
+
+    var running = true;
+    var event = std.mem.zeroes(c.SDL_Event);
+    while (running) {
+        while (c.SDL_PollEvent(&event)) {
+            switch (event.type) {
+                c.SDL_EVENT_QUIT => {
+                    running = false;
+                },
+                else => {},
+            }
+        }
+
+        const surf = try errify(c.SDL_GetWindowSurface(window));
+        try errify(c.SDL_FillSurfaceRect(surf, null, c.SDL_MapSurfaceRGB(surf, 0x5F, 0x5F, 0x5F)));
+        try errify(c.SDL_UpdateWindowSurface(window));
+    }
+}
+
+/// Converts the return value of an SDL function to an error union.
+inline fn errify(value: anytype) error{SdlError}!switch (@typeInfo(@TypeOf(value))) {
+    .bool => void,
+    .pointer, .optional => @TypeOf(value.?),
+    .int => |info| switch (info.signedness) {
+        .signed => @TypeOf(@max(0, value)),
+        .unsigned => @TypeOf(value),
+    },
+    else => @compileError("unerrifiable type: " ++ @typeName(@TypeOf(value))),
+} {
+    return switch (@typeInfo(@TypeOf(value))) {
+        .bool => if (!value) error.SdlError,
+        .pointer, .optional => value orelse error.SdlError,
+        .int => |info| switch (info.signedness) {
+            .signed => if (value >= 0) @max(0, value) else error.SdlError,
+            .unsigned => if (value != 0) value else error.SdlError,
+        },
+        else => comptime unreachable,
+    };
+}
