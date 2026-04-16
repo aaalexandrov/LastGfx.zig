4bddf72ab9660e04f4fd834293f496a4cec78d05
Lolla Bunny <greyalex2003@yahoo.com>
2026-04-17 00:36:41 +0300

    Atomic strong/weak pointer implementation

--- src/rc_ptr.zig
+++ src/rc_ptr.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
-fn emptyDeinit(_: anytype) void {}
+fn emptyDeinit(_: anytype, _: std.mem.Allocator) void {}
 
 fn RcInner(T: type, deinitFn: anytype) type {
     return struct {
         counters: [2]Counter = .{
-            1,
+            0,
             0,
         },
         data: Data = undefined,
@@ -16,23 +16,40 @@ fn RcInner(T: type, deinitFn: anytype) type {
         pub const Self = @This();
 
         fn addRef(self: *Self, comptime ref: u1) void {
-            const prevCounter = @atomicRmw(Counter, &self.counters[ref], .Add, 1, .acq_rel);
-            assert(ref != 0 or prevCounter > 0);
+            _ = @atomicRmw(Counter, &self.counters[ref], .Add, 1, .acq_rel);
         }
 
-        fn decRef(self: *Self, alloc_: std.mem.Allocator, comptime ref: u1) void {
+        fn addRefIfPositive(self: *Self, comptime ref: u1) bool {
+            var counter = self.getRef(ref);
+            while (counter > 0) {
+                if (@cmpxchgWeak(Counter, &self.counters[ref], counter, counter + 1, .acq_rel, .acquire)) |ctr|
+                    counter = ctr
+                else
+                    break;
+            }
+            return counter > 0;
+        }
+
+        fn decRef(self: *Self, alloc: std.mem.Allocator, comptime ref: u1) void {
             const prevCounter = @atomicRmw(Counter, &self.counters[ref], .Sub, 1, .acq_rel);
             assert(ref > 0 or prevCounter > 0);
             if (prevCounter == 1) {
                 if (comptime ref == 0) {
-                    deinitData(&self.data);
-                    alloc_.destroy(self);
+                    // TODO: Should we try to use alloc.resize() to shrink the memory in place?
+                    // if we do and it doesn't fail, then we should use alloc.rawFree() or alloc.resize(new_nen = 0) with the shrunken memory size later when we free the memory
+                    deinitData(&self.data, alloc);
                 }
+                if (self.getRef(1 - ref) == 0)
+                    alloc.destroy(self);
             }
         }
 
-        fn deinitData(data_: *Data) void {
-            deinitFn(data_);
+        fn getRef(self: *const Self, ref: u1) Counter {
+            return @atomicLoad(Counter, &self.counters[ref], .acquire);
+        }
+
+        fn deinitData(data_: *Data, alloc: std.mem.Allocator) void {
+            deinitFn(data_, alloc);
         }
     };
 }
@@ -45,79 +62,131 @@ pub fn RcPtr(T: type, weak: bool, deinitFn: anytype) type {
 
         pub const Data = T;
         pub const Weak: u1 = @intFromBool(weak);
+        pub const AlternativePtr = RcPtr(T, !weak, deinitFn);
+        pub const WeakPtr = if (weak) Self else AlternativePtr;
+        pub const StrongPtr = if (weak) AlternativePtr else Self;
         pub const Self = @This();
 
-        pub fn alloc(alloc_: std.mem.Allocator, data_: Data) !Self {
-            comptime if (weak)
-                @compileError("Cannot allocate data with a weak pointer");
-            const self = Self{
-                .inner = try alloc_.create(Inner),
-            };
-            self.inner.?.* = .{
+        pub fn allocate(self: *Self, alloc: std.mem.Allocator, data_: Data) !void {
+            comptime if (weak) unreachable;
+
+            const inner = try alloc.create(Inner);
+            inner.* = .{
                 .data = data_,
             };
-            return self;
+            self.assignInner(inner, true, alloc);
         }
 
-        pub fn deinit(self: *Self, alloc_: std.mem.Allocator) void {
-            self.assignInner(null, alloc_);
+        pub fn clear(self: *Self, alloc: std.mem.Allocator) void {
+            return self.assignInner(null, true, alloc);
         }
 
-        pub fn reset(self: *Self, alloc_: std.mem.Allocator) void {
-            return self.assignInner(null, alloc_);
+        pub fn assign(self: *Self, rhs: anytype, alloc: std.mem.Allocator) void {
+            const rhsTypeInfo = @typeInfo(@TypeOf(rhs));
+            comptime if (rhsTypeInfo.pointer.child != Self and rhsTypeInfo.pointer.child != AlternativePtr)
+                unreachable;
+
+            const assumeInnerHasRef = comptime (Self.Weak != 0 or @TypeOf(rhs.*).Weak == 0);
+            self.assignInner(rhs.getInner(), assumeInnerHasRef, alloc);
         }
 
-        pub fn assign(self: *Self, rhs: *const Self, alloc_: std.mem.Allocator) void {
-            self.assignInner(rhs.inner, alloc_);
-        }
-
-        pub fn assignPtr(self: *Self, data_: ?*Data, alloc_: std.mem.Allocator) void {
+        pub fn assignPtr(self: *Self, data_: ?*Data, alloc: std.mem.Allocator) void {
+            // here we assume that data was allocated through a RcPtr, and that it's still referenced by one to keep the inner allocation alive
             const inner_: ?*Inner = if (data_) |data__|
                 @fieldParentPtr("data", data__)
             else
                 null;
-            self.assignInner(inner_, alloc_);
-        }
-
-        fn assignInner(self: *Self, inner_: ?*Inner, alloc_: std.mem.Allocator) void {
-            if (inner_) |inner__|
-                inner__.addRef(0);
-            if (self.inner) |selfInner|
-                selfInner.decRef(alloc_, 0);
-            self.inner = inner_;
+            self.assignInner(inner_, true, alloc);
         }
 
         pub fn data(self: *const Self) ?*Data {
+            comptime if (weak) unreachable;
+
             return if (self.inner) |inner_| &inner_.data else null;
         }
 
-        pub fn empty(self: *const Self) bool {
+        pub fn isClear(self: *const Self) bool {
             return self.inner == null;
+        }
+
+        fn getInner(self: *const Self) ?*Inner {
+            return @atomicLoad(?*Inner, &self.inner, .acquire);
+        }
+
+        fn assignInner(self: *Self, inner_: ?*Inner, comptime assumeInnerHasRef: bool, alloc: std.mem.Allocator) void {
+            var effectiveInner = inner_;
+            if (effectiveInner) |inner__| {
+                if (assumeInnerHasRef)
+                    inner__.addRef(Weak)
+                else if (!inner__.addRefIfPositive(Weak))
+                    effectiveInner = null;
+            }
+            const prevInner = @atomicRmw(?*Inner, &self.inner, .Xchg, effectiveInner, .acq_rel);
+            if (prevInner) |prevInner_|
+                prevInner_.decRef(alloc, Weak);
         }
     };
 }
 
-pub fn RcTest(alloc_: std.mem.Allocator) !void {
+pub fn SharedPtr(T: type, deinitFn: anytype) type {
+    return RcPtr(T, false, deinitFn);
+}
+pub fn SharedPtrNoDeinit(T: type) type {
+    return RcPtr(T, false, emptyDeinit);
+}
+pub fn WeakPtr(T: type, deinitFn: anytype) type {
+    return RcPtr(T, true, deinitFn);
+}
+pub fn WeakPtrNoDeinit(T: type) type {
+    return RcPtr(T, true, emptyDeinit);
+}
+
+
+pub fn RcTest(alloc: std.mem.Allocator) !void {
     const expect = std.testing.expect;
     const expectEqual = std.testing.expectEqual;
 
-    const Rc_u32 = RcPtr(u32, false, emptyDeinit);
-    var rc = Rc_u32{};
-    defer rc.deinit(alloc_);
-    try expect(rc.empty());
+    const SharedU32 = SharedPtrNoDeinit(u32);
+    var rc = SharedU32{};
+    defer rc.clear(alloc);
+    try expect(rc.isClear());
     try expect(rc.data() == null);
 
-    rc = try Rc_u32.alloc(alloc_, 44);
+    try rc.allocate(alloc, 44);
     try expect(rc.data() != null);
 
-    var rc1 = Rc_u32{};
-    defer rc1.deinit(alloc_);
-    rc1.assign(&rc, alloc_);
+    var rc1 = SharedU32{};
+    defer rc1.clear(alloc);
+    rc1.assign(&rc, alloc);
     try expectEqual(2, rc1.inner.?.counters[0]);
     try expectEqual(rc.inner, rc1.inner);
 
-    rc.reset(alloc_);
-    try expect(rc.empty());
+    var wc: SharedU32.WeakPtr = WeakPtrNoDeinit(u32){};
+    defer wc.clear(alloc);
+
+    wc.assign(&rc1, alloc);
+    try expectEqual(2, wc.inner.?.counters[0]);
+    try expectEqual(1, wc.inner.?.counters[1]);
+    try expectEqual(rc.inner, wc.inner);
+
+    rc.clear(alloc);
+    try expect(rc.isClear());
+    rc.assign(&rc, alloc);
+    try expect(rc.isClear());
+
+    try expectEqual(1, wc.inner.?.counters[0]);
+    try expectEqual(1, wc.inner.?.counters[1]);
+    try expectEqual(rc1.inner, wc.inner);
+
+    rc.assign(&wc, alloc);
+    try expectEqual(2, wc.inner.?.counters[0]);
+    try expectEqual(1, wc.inner.?.counters[1]);
+    try expectEqual(rc.inner, wc.inner);
+
+    rc.assignPtr(rc.data(), alloc);
+    try expectEqual(2, wc.inner.?.counters[0]);
+    try expectEqual(1, wc.inner.?.counters[1]);
+    try expectEqual(rc.inner, wc.inner);
 }
 
 test "RcPtr" {
